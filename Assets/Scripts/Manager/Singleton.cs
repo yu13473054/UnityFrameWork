481501a00e0c046e48831fc80a080b9e25582a7d
yu13473054 <849460181@qq.com>
2022-07-11 21:01:20 +0800

    增加单例父类

---
+++ Assets/Scripts/Manager/Singleton.cs
@@ -0,0 +1,33 @@
+﻿
+public class Singleton<T> where T : new()
+{
+    private static T _inst;
+    private static readonly object locker = new object();
+
+    public static T Inst
+    {
+        get
+        {
+            if (_inst == null)
+            {
+                lock (locker)
+                {
+                    if (_inst == null)
+                        _inst = new T();
+                }
+            }
+            
+            return _inst;
+        }
+    }
+
+    public virtual void Init()
+    {
+        
+    }
+
+    public virtual void OnDestroy()
+    {
+        _inst = default(T);
+    }
+}
---
+++ Assets/Scripts/Manager/SingletonMono.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _inst;
+    public static T Inst
+    {
+        get
+        {
+            if (!_inst)
+            {
+                _inst = Object.FindObjectOfType<T>();
+                if (!_inst)
+                {
+                    GameObject go = new GameObject(typeof(T).Name);
+                    _inst = go.AddComponent<T>();
+                }
+            }
+            return _inst;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        _inst = this as T;
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _inst = null;
+        Debug.Log($"<{typeof(T).Name}> OnDestroy!");
+    }
+
+}
